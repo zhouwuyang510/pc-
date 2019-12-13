abbfa31ab498efba527cee9ad1af2a8a184719a7
zhouwuyang510 <56677452+zhouwuyang510@users.noreply.github.com>
2019-12-13 13:20:26 +0800

    项目起步 - 整合路由

---
+++ src/router/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+// 导入 vue-router
+import VueRouter from 'vue-router'
+//注册 vue-router
+Vue.use(VueRouter)
+// 实例化
+const routes=[]
+const router = new VueRouter({
+//这里就是路由的配制项
+routes
+})
+export default router
