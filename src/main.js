dd2716cc77f42df473098dde5d745b16c2acff92
zhouwuyang510 <56677452+zhouwuyang510@users.noreply.github.com>
2019-12-13 13:27:42 +0800

    项目起步 - Element-ui整合

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
+// 导入饿了么 ui
+import ElementUI from 'element-ui';
+// 导入饿了么 的样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册一下
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
